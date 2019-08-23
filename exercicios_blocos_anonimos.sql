48dae8eead9211faea35b24099ad764c1bdc64ce
Arthur <rthurmedeiros@gmail.com>
2019-08-23 10:33:08 -0300

    Exercicio 2 da lista de blocos anônimos

--- exercicios_blocos_anonimos.sql
+++ exercicios_blocos_anonimos.sql
@@ -9,6 +9,13 @@ BEGIN
 END;
 
 -- 2. Apresentar a soma de todos os salários para todos os funcionários.
+DECLARE
+    soma Empregado.salario%type;
+BEGIN
+    SELECT sum(salario) INTO soma FROM Empregado;
+    dbms_output.put_line('Soma de todos salários: ' || soma);
+END;
+
 -- 3. Informar o total de projetos já realizados pela empresa.
 -- 4. Informar o total de projetos realizados para um cliente específico.
 -- 5. Informar o código do empregado e imprimir na tela os nomes dos projetos que esse empregado está vinculado.
