c409c54502f02fbd52815e7e7e3c52201e58dc36
Arthur <rthurmedeiros@gmail.com>
2019-08-23 11:30:04 -0300

    Iniciados exercicios de procedures

---
+++ exercicios_procedures.sql
@@ -0,0 +1,28 @@
+-- Com base na estrutura criada em sala de aula do esquema EmpresaProjeto, realizar:
+-- 1. Criar um procedimento que receba como parâmetro o nome do empregado e 
+--    informar na tela o nome do departamento.
+
+-- 2. Criar um procedimento que recebe como parâmetro o nome de um projeto e o 
+--    nome de um cliente e informar se aquele projeto é desse cliente ou não
+
+-- 3. Criar um procedimento que recebe como parâmetro o nome de funcionário e 
+--    retornar como parâmetro de saída o nome do gerente desse funcionário. No
+--    bloco anônimo informar a seguinte mensagem: O funcionário NOME tem como 
+--    gerente o NOMEGERENTE
+
+-- 4. Criar um procedimento que receba como parâmetro o nome do departamento e 
+--    imprimir em tela o nome de todos os funcionários vinculados aquele 
+--    departamento. Imprimir de acordo com exemplo abaixo: 
+--
+--    O departamento nome_departamento apresenta os seguintes funcionários: 
+--    Nome funcionário 1
+--    Nome funcionário 2
+--    ....
+
+-- 5. Criar um procedimento que recebe como parâmetro onome de um projeto e 
+--    imprima na tela o nome de todos os funcionários desse projeto. 
+--
+--    O projeto NOME apresenta os seguintes funcionários
+--    NOME FUNCIONÁRIO 1
+--    NOME FUNCIONÁRIO 2
+--    ...
