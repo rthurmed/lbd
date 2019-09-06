0a2636f6ce8673351c3c17ec2500c261bd6244e9
Arthur <rthurmedeiros@gmail.com>
2019-09-06 09:39:32 -0300

    Enunciados

---
+++ exercicios_functions.sql
@@ -0,0 +1,13 @@
+-- Com base na estrutura criada em sala de aula do esquema EmpresaProjeto, realizar:
+-- Obs.: O retorno deve ser apresentado no bloco anônimo para todas as funções.
+-- 1. Criar uma função que receba como parâmetro a matrícula de um funcionário 
+--    e retornar o total de horas trabalhadas desse funcionárioem todos os projetos. 
+
+-- 2. Criar uma função que recebe como parâmetro a matrícula do funcionário e 
+--    retornar o nome desse funcionário.
+
+-- 3. Criar uma função que recebe o código de um departamento e retornar o 
+--    total de projetos já vinculados nesse departamento.
+
+-- 4. Criar uma função que recebe como parâmetro o nome de um cliente e retorne 
+--    a quantidade total de projetos que esse cliente já realizou.
