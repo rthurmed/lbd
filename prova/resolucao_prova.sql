a46c3e780355bb7f5408d831d5c093458e4f92a2
Arthur <rthurmedeiros@gmail.com>
2019-10-04 09:24:02 -0300

    Exercicio 4 da prova 1

--- prova/resolucao_prova.sql
+++ prova/resolucao_prova.sql
@@ -84,9 +84,8 @@ END;
 -- 4. Faça um procedimento que apresente a seguinte saída:
 -- 
 --    O produto X (nome produto) já foi vendido YY vezes (total de vezes que o produto foi vendido) com as notas fiscais de número: 
--- 
---    Número nota - XX unidades
---    Número nota - XX unidades
+--         Número nota - XX unidades
+--         Número nota - XX unidades
 -- 
 --    Esses são os clientes que adquiriram o produto: 
 --    Nome cliente 1
@@ -94,11 +93,44 @@ END;
 --    ...
 -- 
 --    O produto Y (nome produto) já foi vendido XX vezes(total de vezes que o produto foi vendido) com as notas fiscais de número: 
--- 
---    Número nota - XX unidades
---    Número nota - XX unidades
+--         Número nota - XX unidades
+--         Número nota - XX unidades
 -- 
 --    Esses são os clientes que adquiriram o produto: 
 --    Nome cliente 1
 --    Nome cliente 2
 --    ...
+CREATE OR REPLACE PROCEDURE relatorio_geral_prod AS
+BEGIN
+    FOR prod IN (
+        SELECT p.cod_prod, p.nome, sum(np.quantidade) as vendas
+        FROM Produto p
+        INNER JOIN Nota_Prod np ON p.cod_prod = np.cod_prod
+        GROUP BY p.cod_prod, p.nome
+    ) LOOP
+        dbms_output.put_line('O produto ' || prod.nome || ' já foi vendido ' || prod.vendas || ' vezes com as notas fiscais de número: ');
+        FOR nfprod IN (
+            SELECT cod_nota, quantidade
+            FROM Nota_Prod
+            WHERE cod_prod = prod.cod_prod
+        ) LOOP
+            dbms_output.put_line('     ' || nfprod.cod_nota || ' - ' || nfprod.quantidade || ' unidades');
+        END LOOP;
+        dbms_output.put_line('');
+        dbms_output.put_line('Esses são os clientes que adquiriram o produto:');
+        FOR cliprod IN (
+            SELECT c.nome
+            FROM Nota_Prod np
+            INNER JOIN NotaFiscal nf ON np.cod_nota = nf.cod_nota
+            INNER JOIN Cliente c ON nf.codc = c.codc
+            WHERE np.cod_prod = prod.cod_prod
+        ) LOOP
+            dbms_output.put_line(cliprod.nome);
+        END LOOP;
+        dbms_output.put_line('');
+    END LOOP;
+END;
+
+BEGIN
+    relatorio_geral_prod();
+END;
