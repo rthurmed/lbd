875fd8206db9c4065849dbbe007b1a116564d233
Arthur <rthurmedeiros@gmail.com>
2019-10-04 08:40:17 -0300

    Enunciados prova

---
+++ prova/resolucao_prova.sql
@@ -0,0 +1,37 @@
+-- 1. Faça uma função que receba como parâmetro o código de um produto. A função
+--    deve retornar o total (em valor) já obtivo pela venda desse produto 
+--    (o valor total já vendido em todas as notas). Mostre esse valor no bloco chamador. 
+
+-- 2. Faça um procedimento que apresente a seguinte saída:
+-- 
+--    O cliente X (nome cliente) já realizou a compra dos seguintes produtos:
+-- 
+--    Nome do produto - quantidade total comprada
+--    Nome do produto - quantidade total comprada
+--    ...
+
+-- 3. Faça um procedimento que receba como parâmetro de entrada o número de uma 
+--    nota fiscal e retorne como parâmetro de saída o total de produtos vendidos
+--    nessa nota. Mostrar esse valor no bloco chamador.
+
+-- 4. Faça um procedimento que apresente a seguinte saída:
+-- 
+--    O produto X (nome produto) já foi vendido YY vezes (total de vezes que o produto foi vendido) com as notas fiscais de número: 
+-- 
+--    Número nota - XX unidades
+--    Número nota - XX unidades
+-- 
+--    Esses são os clientes que adquiriram o produto: 
+--    Nome cliente 1
+--    Nome cliente 2
+--    ...
+-- 
+--    O produto Y (nome produto) já foi vendido XX vezes(total de vezes que o produto foi vendido) com as notas fiscais de número: 
+-- 
+--    Número nota - XX unidades
+--    Número nota - XX unidades
+-- 
+--    Esses são os clientes que adquiriram o produto: 
+--    Nome cliente 1
+--    Nome cliente 2
+--    ...
